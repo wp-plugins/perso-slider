643ae4f15c0db86e7a5c26470233194fad373472
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-26 18:24:06 +0000

    perso-slider shortcodes-page
    git-svn-id: https://plugins.svn.wordpress.org/perso-slider/trunk@1146361 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- perso-slider.php
+++ perso-slider.php
@@ -25,6 +25,127 @@ License: GPLv2 or later
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
+// WP Time Menu
+if( !function_exists('WPTime_Add_Admin_Bar_Menu_Aff') ) {
+
+	function WPTime_Add_Admin_Bar_Menu_Aff() {
+
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-parent',
+				'parent'	=>		0,
+				'title' 	=> 		'WP Time',
+				'href' 		=> 		'http://wp-time.com',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-gl',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'Best Gallery & Portfolio WordPress Plugins',
+				'href' 		=> 		'http://j.mp/GL_WPTime',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+		
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-et',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'Collection Of 87 WP Themes For $69 Only',
+				'href' 		=> 		'http://j.mp/ET_WPTime_ref_pl',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-cm',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'WP Themes On Creative Market',
+				'href' 		=> 		'http://j.mp/CM_WPTime',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-tf',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'WP Themes On Themeforest',
+				'href' 		=> 		'http://j.mp/TF_WPTime',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+	
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-cc',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'WP Plugins On Codecanyon',
+				'href' 		=> 		'http://j.mp/CC_WPTime',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-bh',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'Unlimited Web Hosting For $3.95 Only',
+				'href' 		=> 		'http://j.mp/BH_WPTime',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+		$wp_admin_bar->add_menu(
+			array(
+				'id' 		=> 		'wptime-aff-menu-cas',
+				'parent'	=>		'wptime-aff-menu-parent',
+				'title' 	=> 		'Contact And Support',
+				'href' 		=> 		'http://wp-time.com/contact/',
+				'meta'		=>		array('target' => '_blank')
+			)
+		);
+
+	}
+	
+	add_action( 'wp_before_admin_bar_render', 'WPTime_Add_Admin_Bar_Menu_Aff');
+
+}
+
+
+// WP Time Page
+if( !function_exists('WP_Time_Ghozylab_Aff') ) {
+	function WP_Time_Ghozylab_Aff() {
+		add_menu_page( 'WP Time', 'WP Time', 'update_core', 'WP_Time_Ghozylab_Aff', 'WP_Time_Ghozylab_Aff_Page');
+		function WP_Time_Ghozylab_Aff_Page() {
+			?>
+            	<div class="wrap">
+                	<h2>WP Time</h2>
+					<div class="tool-box">
+                		<h3 class="title">Thanks for using our plugins!</h3>
+                    	<p>For more plugins, please visit <a href="http://wp-time.com" target="_blank">WP Time Website</a> and <a href="https://profiles.wordpress.org/qassimdev/#content-plugins" target="_blank">WP Time profile on WordPress</a>.</p>
+                        <p>For contact or support, please visit <a href="http://wp-time.com/contact/" target="_blank">WP Time Contact Page</a>.</p>
+					</div>
+					<div class="tool-box">
+						<h3 class="title">Great WordPress Plugins</h3>
+						<p>Best Gallery & Portfolio WordPress Plugins <a href="http://j.mp/GL_WPTime" target="_blank">Download Now</a>.</p>
+                        <p><a href="http://j.mp/GL_WPTime" target="_blank"><img src="http://content.ghozylab.com/partners/aff/images/global-aff-img.png" width="728" height="90"></a></p>
+					</div>
+                </div>
+			<?php
+		}
+	}
+	add_action( 'admin_menu', 'WP_Time_Ghozylab_Aff' );
+}
+
+
 // Include shortcodes page
 include(plugin_dir_path(__FILE__).'/shortcodes-page.php');
 
